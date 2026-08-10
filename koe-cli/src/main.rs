357ff06ee4060f75e3f4ad72efee4e2b87211ec2
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-08-10 23:05:28 +0900

    feat: implement koe-cli

---
+++ koe-cli/src/main.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, Subcommand};
+use thiserror::Error;
+
+#[derive(Debug, Parser)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    arg_required_else_help = true,
+)]
+pub(crate) struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub(crate) enum Command {}
+
+#[derive(Debug, Error)]
+pub(crate) enum MainError {}
+
+#[tokio::main]
+async fn main() -> Result<(), MainError> {
+    let _ = Cli::parse();
+
+    Ok(())
+}
